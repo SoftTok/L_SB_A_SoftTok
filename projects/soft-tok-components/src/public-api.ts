cd9898cfdd357ed2757e83f9a5efcc0c128e1581
ULFBERTO <binarysoft00101@outlook.es>
2025-04-16 23:08:59 -0500

    Refactor button component structure and update references. Moved ButtonComponent to a new directory structure under 'buttons', updated all related imports and documentation paths accordingly, and removed the old button files.

--- projects/soft-tok-components/src/public-api.ts
+++ projects/soft-tok-components/src/public-api.ts
@@ -6,7 +6,7 @@ export * from './lib/soft-tok-components.service';
 export * from './lib/soft-tok-components.component';
 
 // Export Components
-export * from './stories/components/button/button.component';
+export * from './stories/components/buttons/button/button.component';
 export * from './stories/components/header/header.component';
 export * from './stories/components/page/page.component';
 export * from './stories/user';
--- projects/soft-tok-components/src/stories/components/model-library.module.ts
+++ projects/soft-tok-components/src/stories/components/model-library.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './buttons/button/button.component';
 import { HeaderComponent } from './header/header.component';
 import { PageComponent } from './page/page.component';
 
